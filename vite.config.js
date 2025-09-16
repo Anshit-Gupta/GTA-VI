1f16ca9df677241be97ce08ac84fe006c08e4964
Anshit-Gupta <anshitgupta777@gmail.com>
2025-09-16 21:36:53 +0530

    Add vercel.json and update vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',   // default, but explicit for Vercel
+  },
+  base: './',         // ensures assets load correctly in production
 })
